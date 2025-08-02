5885d26d74d5d6a462b26d93f8b59284c08c3367
may33 <eternalmay33@gmail.com>
2025-08-03 00:01:33 +0200

    added gitignore

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,7 @@
 // simple freehand stroke
 struct Stroke {
     std::vector<ImVec2> points;
-}
+};
 
 static void glfw_error_callback(int error, const char* description) {
     std::cerr << "[GLFW Error " << error << "] " << description << "\n";
